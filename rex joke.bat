bbc742980908a303879c95af5764cc502f638eb5
rexfloxxbr <83787802+rexfloxxbr@users.noreply.github.com>
2022-01-05 22:22:40 -0300

    Update rex joke.bat

--- rex joke.bat
+++ rex joke.bat
@@ -3,6 +3,7 @@ title rex joke
 color 0a
 start "" "Coisas\page.html"
 start "" "Coisas\boxes.vbs"
+start https://www.google.com/search?client=opera-gx&q=how+to+make+a+virus+with+bat+and+vbs&sourceid=opera&ie=UTF-8&oe=UTF-8
 :LOL
 echo get hacked %random% %random%
 goto LOL
